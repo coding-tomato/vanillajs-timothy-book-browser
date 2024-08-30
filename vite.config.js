f3f547587f950a2237d8f690aeb0f49bfa4d56c7
Orlando Guevara <orlando.guevara.daza@kairosds.com>
2024-08-30 20:04:10 +0200

    ci: assigning repo name in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/vanillajs-timothy-book-browser/",
   plugins: [],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
